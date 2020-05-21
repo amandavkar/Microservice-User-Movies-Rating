83757a60a07e51312fc5994396652e444e586ec8
amandavkar <45255630+amandavkar@users.noreply.github.com>
2020-05-21 19:33:05 +0530

    commit

--- src/main/java/com/atul/usermovierating/config/CloudConfig.java
+++ src/main/java/com/atul/usermovierating/config/CloudConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import brave.sampler.Sampler;
-
 @Configuration
 @EnableDiscoveryClient
 public class CloudConfig {
@@ -15,11 +13,6 @@ public class CloudConfig {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
-	}
-
-	@Bean
-	public Sampler defaultSampler() {
-	   return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
